63d8f3dd2f714a6ad08b9ecc9fb04a652b2a4afc
alegarcro <alegarcro@myuax.com>
2024-11-05 11:30:02 +0100

    Commit 1

---
+++ Ejercicio 4/Ej4.h
@@ -0,0 +1,10 @@
+#ifndef EJ4_H
+#define EJ4_H
+
+
+void promocion();
+
+
+
+
+#endif //EJ4_H
---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "Ejercicio1/EJ1.h"
+#include "Ejercicio 2/EJ2.h"
+#include "Ejercicio 3/EJ3.h"
+#include "Ejercicio 4/EJ4.h"
+
+
+int main() {
+
+    promocion();
+    identificacion();
+    semana();
+    promedio();
+
+    return 0;
+}
