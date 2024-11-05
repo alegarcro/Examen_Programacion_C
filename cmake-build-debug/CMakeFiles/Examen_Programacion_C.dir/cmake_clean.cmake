4784ea3fd5f9b80c1b39ed218f0f926d56da26ee
alegarcro <alegarcro@myuax.com>
2024-11-05 11:31:08 +0100

    Commit Final

---
+++ cmake-build-debug/CMakeFiles/Examen_Programacion_C.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Examen_Programacion_C.dir/Ejercicio1/Identificacion.cpp.obj"
+  "CMakeFiles/Examen_Programacion_C.dir/Ejercicio1/Identificacion.cpp.obj.d"
+  "CMakeFiles/Examen_Programacion_C.dir/Ejercicio_2/Diasemana.cpp.obj"
+  "CMakeFiles/Examen_Programacion_C.dir/Ejercicio_2/Diasemana.cpp.obj.d"
+  "CMakeFiles/Examen_Programacion_C.dir/main.cpp.obj"
+  "CMakeFiles/Examen_Programacion_C.dir/main.cpp.obj.d"
+  "Examen_Programacion_C.exe"
+  "Examen_Programacion_C.exe.manifest"
+  "Examen_Programacion_C.pdb"
+  "libExamen_Programacion_C.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Examen_Programacion_C.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Examen_Programacion_C.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Examen_Programacion_C.
+# This may be replaced when dependencies are built.
